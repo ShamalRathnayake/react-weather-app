c6b6a5105237d80e9efa103ccf2e750462a95cab
ShamalRathnayake <shamalkavinda44@gmail.com>
2020-12-26 23:52:51 +0530

    code refactored

--- src/App.js
+++ src/App.js
@@ -1,56 +1,29 @@
 import React, { lazy, Suspense, useState } from 'react';
 import './App.css';
 import Loading from './components/loading/Loading';
-import BottomDrawer from './components/bottomDrawer/BottomDrawer';
 import getWeather from './apiService/apiService';
 
-require('dotenv').config();
-
-
-
-const Map = lazy(() => import('./components/map/Map'))
-
-
-
-const style = {
-  App: {
-    maxHeight: "100vh",
-    maxWidth: "100vw",
-    overflow: "hidden"
-  },
-
-  bg: {
-    backgroundColor: "#101010",
-    height: "100vh",
-    width: "100vw",
-    position: "relative"
-  }
-};
-
+const MapView = lazy(() => import('./components/map/Map.view'))
 
 
 function App() {
 
-  const [openDrawer, setOpenDrawer] = useState(false)
   const [weather, setWeather] = useState({})
 
-
   const fetchWeatherData = async (lat, lon) => {
+    await setWeather({})
     const weatherData = await getWeather(lat, lon)
     setWeather(weatherData)
   }
 
 
   return (
-    <div className="App" style={style.App}>
-
-      <div style={style.bg}>
+    <div className="App">
+      <div className="background">
         <Suspense fallback={<Loading />}>
-          <Map setOpenDrawer={setOpenDrawer} fetchWeatherData={fetchWeatherData} />
+          <MapView weather={weather} fetchWeatherData={fetchWeatherData} />
         </Suspense>
       </div>
-      <BottomDrawer openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} weather={weather} />
-
     </div>
   );
 }
